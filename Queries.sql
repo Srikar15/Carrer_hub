904f55e0dd4849bfd105d5b27f4c55b965cdca09
Srikar15 <91366853+Srikar15@users.noreply.github.com>
2025-04-01 10:39:09 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,27 @@
-5.
+5.SELECT 
+    j.JobTitle, 
+    COUNT(a.ApplicationID) AS ApplicationCount
+FROM Jobs j
+LEFT JOIN Applications a ON j.JobID = a.JobID
+GROUP BY j.JobTitle;
+
+6. SELECT 
+    j.JobTitle, 
+    c.CompanyName, 
+    j.JobLocation, 
+    j.Salary
+FROM Jobs j
+JOIN Companies c ON j.CompanyID = c.CompanyID
+WHERE j.Salary BETWEEN @MinSalary AND @MaxSalary;
+
+7. SELECT 
+    j.JobTitle, 
+    c.CompanyName, 
+    a.ApplicationDate
+FROM Applications a
+JOIN Jobs j ON a.JobID = j.JobID
+JOIN Companies c ON j.CompanyID = c.CompanyID
+WHERE a.ApplicantID = @ApplicantID
+ORDER BY a.ApplicationDate DESC;
+
+8.
